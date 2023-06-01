09e264bec468d3eefce5f9af55aed87e69a45947
Ormeline <ormelineluyengi@outlook.com>
2023-06-01 16:53:26 +0100

    10_adding_a_note_ui.

--- web-frontend-applications/notes-app/notesView.js
+++ web-frontend-applications/notes-app/notesView.js
@@ -1,22 +1,41 @@
 // notesView.js
 
 class NotesView {
-    constructor(model) {
-      this.model = model;
-      this.mainContainerEl = document.querySelector('#main-container');
-    }
-    
-    displayNotes() {
-      const notes = this.model.getNotes()
-  
-      // For each note, create and append a new element on the main container
-      notes.forEach(note => {
-        const noteEl = document.createElement('div');
-        noteEl.textContent = note;
-        noteEl.className = 'note';
-        this.mainContainerEl.append(noteEl);
-      })
-    }
+  constructor(model) {
+    this.model = model;
+    this.mainContainerEl = document.querySelector('#main-container');
+
+    // Add event listener to the "Add Note" button
+    document.querySelector('#add-note-button').addEventListener('click', () => {
+      // Get the value from the input field
+      const newNote = document.querySelector('#add-note-input').value;
+      // Call the addNewNote method with the new note
+      this.addNewNote(newNote);
+    });
   }
+
+  // Method to add a new note
+  addNewNote(newNote) {
+    // Call the addNote method of the model with the new note
+    this.model.addNote(newNote);
+    // Call the displayNotes method to update the view
+    this.displayNotes();
+  }
+    
+  // Method to display notes
+  displayNotes() {
+    // Get the notes from the model
+    const notes = this.model.getNotes();
   
-  module.exports = NotesView;
+    // For each note, create and append a new element to the main container
+    notes.forEach(note => {
+      const noteEl = document.createElement('div');
+      noteEl.textContent = note;
+      noteEl.className = 'note';
+      this.mainContainerEl.append(noteEl);
+    });
+  }
+}
+
+// Export the NotesView class
+module.exports = NotesView;
--- web-frontend-applications/notes-app/notesView.test.js
+++ web-frontend-applications/notes-app/notesView.test.js
@@ -7,32 +7,46 @@ const fs = require('fs');
 const NotesModel = require('./notesModel');
 const NotesView = require('./notesView');
 
+// Set up the test suite for Notes view
 describe('Notes view', () => {
+  // Test case: Displaying two notes
   it('displays two notes', () => {
+    // Load the HTML file into the DOM
     document.body.innerHTML = fs.readFileSync('./index.html');
 
-    //1. setting up model and view
+    // 1. Setting up the model and view
     const model = new NotesModel();
     const view = new NotesView(model);
+
+    // Add two notes to the model
     model.addNote('first note');
     model.addNote('second note');
 
-    //2. Display the notes on the page
+    // 2. Display the notes on the page
     view.displayNotes();
 
-    //3. There should be 2 ndiv.note on the page
+    // 3. There should be 2 div.note elements on the page
     expect(document.querySelectorAll('div.note').length).toEqual(2);
   });
 
-  // it('adds a new paragraph', () => {
-  //   //Arrange
-  //   document.body.innerHTML = fs.readFileSync('./index.html'); //taking the concept of the index.html file. 
-  //   const view = new View(); //this instatiates the View class which contains the code we want to test
-    
-  // //Act  
-  //   view.addParagraph();// this is the behaviour that we want to test
-
-  //   //Assert
-  //   expect(document.querySelectorAll('p').length).toBe(3);
-  // });
+  // Test case: Adding a new note
+  it('adds a new note', () => {
+    // Load the HTML file into the DOM
+    document.body.innerHTML = fs.readFileSync('./index.html');
+  
+    const model = new NotesModel();
+    const view = new NotesView(model);
+  
+    // 1. Fill the input with a new note
+    const input = document.querySelector('#add-note-input');
+    input.value = 'A new test note';
+  
+    // 2. Click the button to add the note
+    const button = document.querySelector('#add-note-button');
+    button.click();
+  
+    // 3. The note should be on the page
+    expect(document.querySelectorAll('div.note').length).toEqual(1);
+    expect(document.querySelectorAll('div.note')[0].textContent).toEqual('A new test note');
+  });
 });
